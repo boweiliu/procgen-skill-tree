fdd170469c01844988b7151adf557e6b913fce66
Bowei Liu <liubowei@gmail.com>
2020-10-11 10:03:58 -0700

    yay we can see stuff

--- procgen-skill-tree/src/components/Area.tsx
+++ procgen-skill-tree/src/components/Area.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import "./Area.css";
 
 export function Area(props: { data: string }) {
   let { data } = props;
   return (
     <div>
       <span> got data: {data.toString()} </span>
-      <canvas />
+      <canvas
+        width={window.innerWidth * .875}
+        height={window.innerHeight  * .875}
+        onClick={(e) => {
+          alert(e.clientX);
+        }}
+      />
     </div>
   );
 }
