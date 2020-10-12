3706946162d4865b1338e53966d4aaa9351c5e6b
Bowei Liu <liubowei@gmail.com>
2020-10-12 10:51:53 -0700

    fixup

--- procgen-skill-tree/src/johnfn_library/entity.ts
+++ procgen-skill-tree/src/johnfn_library/entity.ts
@@ -280,8 +280,8 @@ export class Entity {
 
   public set texture(value: Texture) { this.sprite.texture = value; }
 
-  public set mask(value: Container | MaskData) { this.sprite.mask = value; }
-  public get mask(): Container | MaskData { return this.sprite.mask; }
+  public set mask(value: Container | MaskData | null) { this.sprite.mask = value; }
+  public get mask(): Container | MaskData | null { return this.sprite.mask; }
 
   public get scale(): Vector2 { return new Vector2({ x: this.sprite.scale.x, y: this.sprite.scale.y }); }
   public set scale(value: Vector2) {
