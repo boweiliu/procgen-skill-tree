20868442231b3583a468d348a21853ac207e8a48
Bowei Liu <liubowei@gmail.com>
2020-10-12 18:13:39 -0700

    rework names

--- procgen-skill-tree/src/pixi/TopoLayer.ts
+++ procgen-skill-tree/src/pixi/TopoLayer.ts
@@ -15,54 +15,36 @@ export class TopoTemplate {
 
   public nodes: TopoNode[] = [];
 
+  doStuff() {}
+
   constructor(id: string, frames: Topo3Frames, random: HashState) {
     this.id = id;
     this.frames = frames;
     this.orientation = frames.out.orientation;
 
     // start constructing the guy
-    connectCorner(this.frames.out.corners["00"], this.frames.in0.corners["00"]);
-    connectCorner(this.frames.out.corners["11"], this.frames.in1.corners["00"]);
+    // opposite corners
+    connectCorner(this.frames.out.corners.I, this.frames.in0.corners.I);
+    connectCorner(this.frames.out.corners.D, this.frames.in1.corners.D);
+    // the other 2 outside corners
+    connectCorner(this.frames.out.corners.S, this.frames.in0.corners.L);
+    connectCorner(this.frames.out.corners.L, this.frames.in1.corners.S);
+    // center corners
+    connectCorner(this.frames.in0.corners.S, this.frames.in1.corners.I);
+    connectCorner(this.frames.in0.corners.D, this.frames.in1.corners.L);
 
-    if (this.orientation == "=") {
-      connectCorner(
-        this.frames.out.corners["01"],
-        this.frames.in0.corners["01"]
-      );
-      connectCorner(
-        this.frames.out.corners["10"],
-        this.frames.in1.corners["10"]
-      );
-      connectMiddleCorner(
-        this.frames.in0.corners["10"],
-        this.frames.in1.corners["00"]
-      );
-      connectMiddleCorner(
-        this.frames.in0.corners["11"],
-        this.frames.in1.corners["01"]
-      );
-    } else if (this.orientation == "0") {
-      connectCorner(
-        this.frames.out.corners["01"],
-        this.frames.in1.corners["01"]
-      );
-      connectCorner(
-        this.frames.out.corners["10"],
-        this.frames.in0.corners["10"]
-      );
-      connectMiddleCorner(
-        this.frames.in0.corners["01"],
-        this.frames.in1.corners["00"]
-      );
-      connectMiddleCorner(
-        this.frames.in0.corners["11"],
-        this.frames.in1.corners["10"]
-      );
-    }
+    // make some midpoints
+
+    // connect some midpoints
+    // out.IS , in0.IL
+    // in0.SD, in1.IL
+    // in1.SD, out.LD
+    // out.IL, in0.IS, in1.IS
+    // out.SD, in0.LD, in1.LD
+
   }
 }
 function connectCorner(x: any, y: any) {}
-function connectMiddleCorner(x: any, y: any) {}
 
 export type FrameOrientation = "=" | "0";
 export class TopoFrame {
@@ -70,24 +52,28 @@ export class TopoFrame {
   public orientation!: FrameOrientation;
 
   public corners!: {
-    "00": TopoNode;
-    "01": TopoNode;
-    "10": TopoNode;
-    "11": TopoNode;
+    I: TopoNode;
+    L: TopoNode;
+    S: TopoNode;
+    D: TopoNode;
   };
 
   /**
-   * 00 ----- 10
-   * |         |
-   * 01 ----- 11
+   * 
+   * I -----   L       I  -  S
+   * |         |       |     |
+   * S  -----  D  or   |     |
+   *                   |     |
+   *                   L  -  D
+   * 
    */
   constructor(id: string, orientation: FrameOrientation) {
     this.id = id;
     this.corners = {
-      "00": new TopoNode(id + "-N00"),
-      "01": new TopoNode(id + "-N01"),
-      "10": new TopoNode(id + "-N10"),
-      "11": new TopoNode(id + "-N11"),
+      I: new TopoNode(id + "-NI"),
+      L: new TopoNode(id + "-NL"),
+      S: new TopoNode(id + "-NS"),
+      D: new TopoNode(id + "-ND"),
     };
     this.orientation = orientation;
   }
