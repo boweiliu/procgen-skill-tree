25b563fa201c3ff022a8cbbd2bd10c927613e163
Bowei Liu <liubowei@gmail.com>
2020-12-05 00:11:17 -0800

    update with newer johnfn library version

--- procgen-skill-tree/src/johnfn_library/README.md
+++
@@ -1,1 +0,0 @@
-Taken from https://github.com/johnfn/ts-game-starter -- all credit to @johnfn
--- procgen-skill-tree/src/johnfn_library/environment.ts
+++
@@ -1,7 +0,0 @@
-export const IS_PRODUCTION =
-  !window.location.href.includes("localhost") ||
-  window.location.href.includes("debug=false");
-export const IS_DEVELOPMENT = !IS_PRODUCTION;
-export const IS_DEBUG =
-  (IS_DEVELOPMENT || window.location.href.includes("debug=true")) &&
-  !window.location.href.includes("debug=false");
