d25722452b8ef830b4f51d6db40abd40736ee1d4
Bowei Liu <liubowei@gmail.com>
2020-12-19 17:52:04 -0800

    fix tsc

--- frontend/src/components/NodeDetail.tsx
+++ frontend/src/components/NodeDetail.tsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import "./NodeDetail.css";
 const a = [1, 2, 3, 4, 5, 6];
 
-export function NodeDetail({ focusedNode }: { focusedNode: any }) {
-  const [history, setHistory] = useState([]);
+export function NodeDetail({ focusedNode }: { focusedNode: {chunk: any, node: any} }) {
+  const [history, setHistory] = useState<{ chunk: any, node: any }[]>([]);
   console.log(focusedNode);
   useEffect(() => {
     setHistory((history) => [...history, focusedNode]);
@@ -13,13 +13,13 @@ export function NodeDetail({ focusedNode }: { focusedNode: any }) {
       <div className="layout">
         <h1>Current</h1>
         <h3>Chunk: {focusedNode.chunk?.id}</h3>
-        <h3>X: {focusedNode.node.location.x}</h3>
-        <h3>Y: {focusedNode.node.location.y}</h3>
+        <h3>X: {focusedNode.node?.location?.x}</h3>
+        <h3>Y: {focusedNode.node?.location?.y}</h3>
         <h2>Previous</h2>
         {history.map(({ chunk, node }, i) => {
           return (
             <div key={i}>
-              Chunk {chunk.id} at {node.location.x},{node.location.y}
+              Chunk {chunk?.id} at {node?.location?.x},{node?.location?.y}
             </div>
           );
         })}
