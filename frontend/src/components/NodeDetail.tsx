0de6c89aa8b13b35623207f9ddd8c61aff7acf01
Alice Zhao <alice.zhao@berkeley.edu>
2020-12-19 18:05:38 -0800

    vector fix

--- frontend/src/components/NodeDetail.tsx
+++ frontend/src/components/NodeDetail.tsx
@@ -6,6 +6,7 @@ export function NodeDetail({ focusedNode }: { focusedNode: {chunk: any, node: an
   const [history, setHistory] = useState<{ chunk: any, node: any }[]>([]);
   console.log(focusedNode);
   useEffect(() => {
+    if (focusedNode.chunk === null) return;
     setHistory((history) => [...history, focusedNode]);
   }, [focusedNode]);
   return (
@@ -13,16 +14,20 @@ export function NodeDetail({ focusedNode }: { focusedNode: {chunk: any, node: an
       <div className="layout">
         <h1>Current</h1>
         <h3>Chunk: {focusedNode.chunk?.id}</h3>
-        <h3>X: {focusedNode.node?.location?.x}</h3>
-        <h3>Y: {focusedNode.node?.location?.y}</h3>
+        <h3>X: {focusedNode.node?.x}</h3>
+        <h3>Y: {focusedNode.node?.y}</h3>
         <h2>Previous</h2>
-        {history.map(({ chunk, node }, i) => {
-          return (
-            <div key={i}>
-              Chunk {chunk?.id} at {node?.location?.x},{node?.location?.y}
-            </div>
-          );
-        })}
+        {history
+          
+          .map(({ chunk, node }, i) => {
+              return (
+                <div key={i}>
+                  Chunk {chunk?.id} at {node?.x},{node?.y}
+                </div>
+              );
+            })
+          
+          .reverse()}
       </div>
     </>
   );
