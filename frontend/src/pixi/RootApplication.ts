155a5dd3b2cdb96bad2644292ae99462cb1735c9
Bowei Liu <liubowei@gmail.com>
2020-12-23 02:59:42 -0800

    resolve tsconfig change

--- frontend/src/pixi/RootApplication.ts
+++ frontend/src/pixi/RootApplication.ts
@@ -20,6 +20,7 @@ type RootApplicationProps = {}
 export class RootApplication {
   state!: RootApplicationState;
   props!: DeepReadonly<RootApplicationProps>;
+  container!: Pixi.Container;
 
   /* children */
   // Contains HUD, and other entities that don't move when game camera moves
@@ -43,71 +44,56 @@ export class RootApplication {
   /**
    * Need to provide config to set up the pixi canvas
    */
-  constructor(config?: Partial<Config>) {
-    assertOnlyCalledOnce("Base application constructor");
-    this.config = Object.assign({}, defaultConfig, config);
+  constructor(args: any) {
+    // assertOnlyCalledOnce("Base application constructor");
 
-    const appSize = BaseApplication.appSizeFromWindowSize(new Vector2(
-      this.config.originalWindowWidth, this.config.originalWindowHeight
-    ));
-    this.originalAppWidth = appSize.x;
-    this.originalAppHeight = appSize.y;
+    // this.app = new Pixi.Application({
+    //   width: this.originalAppWidth, // both are ignored - see resize() below
+    //   height: this.originalAppHeight,
+    //   antialias: true, // both about the same FPS, i get around 30 fps on 1600 x 900
+    //   transparent: true, // true -> better fps?? https://github.com/pixijs/pixi.js/issues/5580
+    //   resolution: window.devicePixelRatio || 1, // lower -> more FPS but uglier
+    //   // resolution: 0.5,
+    //   // resolution: 2,
+    //   autoDensity: true,
+    //   powerPreference: "low-power", // the only valid one for webgl
+    //   backgroundColor: 0xffffff, // immaterial - we recommend setting color in backdrop graphics
+    // });
 
-    this.app = new Pixi.Application({
-      width: this.originalAppWidth, // both are ignored - see resize() below
-      height: this.originalAppHeight,
-      antialias: true, // both about the same FPS, i get around 30 fps on 1600 x 900
-      transparent: true, // true -> better fps?? https://github.com/pixijs/pixi.js/issues/5580
-      resolution: window.devicePixelRatio || 1, // lower -> more FPS but uglier
-      // resolution: 0.5,
-      // resolution: 2,
-      autoDensity: true,
-      powerPreference: "low-power", // the only valid one for webgl
-      backgroundColor: 0xffffff, // immaterial - we recommend setting color in backdrop graphics
-    });
+    // this.stage = this.app.stage;
+    // this.stage.sortableChildren = true;
 
-    this.stage = this.app.stage;
-    this.stage.sortableChildren = true;
+    // // this.fixedCameraStage = new Pixi.Sprite();
+    // // this.fixedCameraStage.zIndex = 1;
+    // // this.fixedCameraStage.sortableChildren = true;
+    // // this.stage.addChild(this.fixedCameraStage);
 
-    // this.fixedCameraStage = new Pixi.Sprite();
-    // this.fixedCameraStage.zIndex = 1;
-    // this.fixedCameraStage.sortableChildren = true;
-    // this.stage.addChild(this.fixedCameraStage);
+    // // this.actionStage = new Pixi.Sprite();
+    // // this.actionStage.zIndex = 0;
+    // // this.actionStage.sortableChildren = true;
+    // // this.stage.addChild(this.actionStage);
 
-    // this.actionStage = new Pixi.Sprite();
-    // this.actionStage.zIndex = 0;
-    // this.actionStage.sortableChildren = true;
-    // this.stage.addChild(this.actionStage);
+    // // this.backdropStage = new Pixi.Sprite();
+    // // this.backdropStage.zIndex = -1;
+    // // this.backdropStage.sortableChildren = true;
+    // // this.stage.addChild(this.backdropStage);
 
-    // this.backdropStage = new Pixi.Sprite();
-    // this.backdropStage.zIndex = -1;
-    // this.backdropStage.sortableChildren = true;
-    // this.stage.addChild(this.backdropStage);
+    // // this.keyboard = new KeyboardState();
+    // // this.app.ticker.add(() => {
+    // //   this.keyboard.update();
+    // // })
 
-    // this.keyboard = new KeyboardState();
-    // this.app.ticker.add(() => {
-    //   this.keyboard.update();
-    // })
+    // // this.fpsTracker = new FpsTracker();
+    // // this.app.ticker.add((delta) => {
+    // //   // delta should be approximately equal to 1
+    // //   this.fpsTracker.tick(delta);
+    // // })
 
-    // this.fpsTracker = new FpsTracker();
-    // this.app.ticker.add((delta) => {
-    //   // delta should be approximately equal to 1
-    //   this.fpsTracker.tick(delta);
-    // })
+    // let pointNodeTexture = new Lazy(() => generatePointNodeTexture(this.app.renderer));
 
-    let pointNodeTexture = new Lazy(() => generatePointNodeTexture(this.app.renderer));
-
-    // test
-    // createBunnyExample({ parent: this.actionStage, ticker: this.app.ticker, x: this.app.screen.width / 2, y: this.app.screen.height / 2 });
-    this.app.ticker.add((delta) => this.baseGameLoop(delta));
-  }
-
-  /**
-   * Please only call once!!
-   * Usage: const container = useRef<HTMLDivElement>(null); useEffect(() => { application.register(container.current!); }, []);
-   */
-  public register(curr: HTMLDivElement) {
-    curr.appendChild(this.app.view);
+    // // test
+    // // createBunnyExample({ parent: this.actionStage, ticker: this.app.ticker, x: this.app.screen.width / 2, y: this.app.screen.height / 2 });
+    // this.app.ticker.add((delta) => this.baseGameLoop(delta));
   }
 
   // render(props: {
@@ -115,10 +101,5 @@ export class RootApplication {
   //   pixiComponentState: PixiComponentState,
   // }) { }
 
-  baseGameLoop(delta: number) {
-    
-
-
-  }
 }
 
