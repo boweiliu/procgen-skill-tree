2eee1ee92a8799d7b27804faf9c8a7f8f633aede
Alice Zhao <alice.zhao@berkeley.edu>
2020-12-26 02:35:58 -0800

    children lifecycle?

--- frontend/src/pixi/components/LifecycleHandler.ts
+++ frontend/src/pixi/components/LifecycleHandler.ts
@@ -4,12 +4,35 @@ type Props = {};
 
 type State = {};
 
+type ChildInstructions<
+  ChildInstanceType,
+  ChildPropsType extends Props,
+  ParentPropsType extends Props,
+  ParentStateType extends State
+> = {
+  childClass: new (props: ChildPropsType) => ChildInstanceType;
+  instance?: ChildInstanceType;
+  propsFactory: (
+    parentProps: ParentPropsType,
+    parentState: ParentStateType
+  ) => ChildPropsType;
+};
+
 class LifecycleHandlerBase<P extends Props, S extends State> {
   state!: S;
+  children: ChildInstructions<any, any, P, S>[];
+  container!: PIXI.Container;
   constructor(props: P) {
     this.staleProps = props;
+    this.children = []; // child implementation should register child class and props factory in constructor?
   }
   _didConstruct(props: P) {
+    this.children.forEach((child) => {
+      child.instance = new child.childClass(
+        child.propsFactory(props, this.state)
+      );
+      this.container.addChild(child.instance.container);
+    });
     this.renderSelf(props);
     this.didMount();
   }
@@ -24,6 +47,7 @@ class LifecycleHandlerBase<P extends Props, S extends State> {
     this._setStaleProps(nextProps);
     new Promise((resolve) => resolve(this.didUpdate()));
   }
+
   protected staleProps: P;
   fireStateUpdaters() {}
   didMount() {}
@@ -31,7 +55,11 @@ class LifecycleHandlerBase<P extends Props, S extends State> {
   shouldUpdate(staleProps: P, staleState: S, nextProps: P, state: S): boolean {
     return true;
   }
-  updateChildren(nextProps: P) {}
+  updateChildren(nextProps: P) {
+    this.children.forEach(({ instance, propsFactory }) => {
+      instance._update(propsFactory(nextProps, this.state));
+    });
+  }
   renderSelf(nextProps: P) {}
   didUpdate() {}
   _setStaleProps(nextProps: P) {
@@ -51,6 +79,7 @@ export const LifecycleHandler = new Proxy(LifecycleHandlerBase, {
 /**
  * First render:
  * constructor
+ * renderChildren?
  * renderSelf
  * didMount
  *
@@ -60,6 +89,7 @@ export const LifecycleHandler = new Proxy(LifecycleHandlerBase, {
  * updateSelf
  * shouldUpdate(props,state)?
  * updateChildren
+ * children._update
  * renderSelf
  * didUpdate
  * staleProps = props
