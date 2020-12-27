c357e8f85199aa55b17f22433278c44ecb099363
Alice Zhao <alice.zhao@berkeley.edu>
2020-12-26 20:18:06 -0800

    quest scoring

--- frontend/src/components/QuestProgress.tsx
+++ frontend/src/components/QuestProgress.tsx
@@ -15,6 +15,16 @@ type Props = {
 
   score: GameState["playerSave"]["score"];
 };
+type QuestScoreDetails = {
+  total: number;
+  scoreComponents: ScoreComponent[];
+};
+type ScoreComponent = {
+  inputAmount: number;
+  inputTitle: string;
+  outputScore: number;
+  outputDescription: string;
+};
 export default React.memo(QuestProgressComponent);
 function QuestProgressComponent({
   activeQuest,
@@ -25,26 +35,56 @@ function QuestProgressComponent({
   updaters,
   score,
 }: Props) {
+  const [scoreDetails, setScoreDetails] = useState<QuestScoreDetails>({
+    total: 0,
+    scoreComponents: [],
+  });
   const isQuestComplete =
     activeQuest &&
     (playerResourceAmounts?.[activeQuest.resourceType] || 0) >=
       activeQuest.resourceAmount;
+  function calculateQuestScoreDetails() {
+    const questScore: { scoreComponents: ScoreComponent[] } = {
+      scoreComponents: [],
+    };
+    questScore.scoreComponents.push({
+      inputAmount: 1,
+      inputTitle: "quest completed",
+      outputScore: 5,
+      outputDescription: "",
+    });
+    questScore.scoreComponents.push({
+      inputAmount: numBatches,
+      inputTitle: "nodes allocated",
+      outputScore: Math.floor(10 / (numBatches + 1)),
+      outputDescription: "efficiency",
+    });
+    const total = questScore.scoreComponents.reduce(
+      (subtotal, prev) => subtotal + prev.outputScore,
+      0
+    );
+    console.log(total);
+    return { ...questScore, total };
+  }
   const doClaimReward = () => {
-    // window.alert("your score increased by 831!");
-    // const questScore = efficiency;
-    const questScore = 831;
+    const questScore = calculateQuestScoreDetails();
     updaters.score.enqueueUpdate((lastScore) => {
-      console.log({ score, lastScore });
+      console.log({ score, lastScore, questScore });
       console.log("updating score");
-      return lastScore + questScore;
+      return lastScore + questScore.total;
     });
     updaters.activeQuest.enqueueUpdate(() => {
       return undefined;
     });
+    setScoreDetails(questScore);
+  };
+  const handleStartQuest = () => {
+    // setOldPoints()
+    createQuestCb();
   };
   return (
     <>
-      {<Score score={score} />}
+      {<Score score={score} scoreDetails={scoreDetails} />}
       {activeQuest === undefined ? (
         <>
           <h2>
@@ -54,12 +94,7 @@ function QuestProgressComponent({
           </h2>
           <br></br>
           <br></br>
-          <button
-            className="button"
-            onClick={() => {
-              createQuestCb();
-            }}
-          >
+          <button className="button" onClick={handleStartQuest}>
             Get a quest
           </button>
           <br></br>
@@ -122,21 +157,46 @@ function QuestProgressComponent({
   );
 }
 
-function Score({ score }: { score: number }) {
-  const [lastScore, setLastScore] = useState(0);
-  const [delta, setDelta] = useState(0);
+function Score({
+  scoreDetails,
+  score,
+}: {
+  score: number;
+  scoreDetails: QuestScoreDetails;
+}) {
+  const [viewed, setViewed] = useState(true);
   useEffect(() => {
-    console.log(`new score received: ${score}`);
-    setLastScore((lastScore) => {
-      setDelta(() => score - lastScore);
-      return score;
-    });
-  }, [score]);
-
+    console.log({ scoreDetails });
+    if (scoreDetails.total === 0) return;
+    setViewed(false);
+  }, [scoreDetails]);
   return (
     <>
-      {!!delta && <h6>Your score increased by {delta}!</h6>}
-      {!!score && <h3>Score: {score}</h3>}
+      {!viewed && (
+        <>
+          <h3>Score: {score}</h3>
+          {!!scoreDetails.total && (
+            <h4>Your score increased by {scoreDetails.total}!</h4>
+          )}
+          {scoreDetails.scoreComponents.map((scoreComponent) => {
+            return (
+              <React.Fragment key={scoreComponent.inputTitle}>
+                <h5>
+                  {scoreComponent.outputDescription &&
+                    scoreComponent.outputDescription + ": "}
+                  {formatDelta(scoreComponent.outputScore)} score for{" "}
+                  {scoreComponent.inputAmount} {scoreComponent.inputTitle}
+                </h5>
+              </React.Fragment>
+            );
+          })}
+          <button onClick={() => setViewed(true)}>OK</button>
+        </>
+      )}
     </>
   );
+}
+
+function formatDelta(d: number): string {
+  return d < 0 ? d.toString() : "+" + d.toString();
 }
