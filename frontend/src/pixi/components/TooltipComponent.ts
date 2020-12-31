9ccb84330ca70b0f25779a7a2c5d3052dfe98be4
Bowei Liu <liubowei@gmail.com>
2020-12-31 15:24:22 -0800

    render tooltip component

--- frontend/src/pixi/components/TooltipComponent.ts
+++ frontend/src/pixi/components/TooltipComponent.ts
@@ -1,8 +1,14 @@
 import * as Pixi from "pixi.js";
+import { PixiPointFrom } from "../../lib/pixi/pixify";
+import { Vector2 } from "../../lib/util/geometry/vector2";
 import { engageLifecycle, LifecycleHandlerBase } from "./LifecycleHandler";
 
 type Props = {
-
+  args: {
+  },
+  text: string;
+  position: Vector2;
+  visible: boolean;
 }
 
 type State = {} 
@@ -11,14 +17,37 @@ class TooltipComponent extends LifecycleHandlerBase<Props, State> {
   public container: Pixi.Container;
   public state: State;
 
+  private text: Pixi.Text;
+
   constructor(props: Props) {
     super(props);
     this.container = new Pixi.Container();
     this.state = {};
+
+    this.text = new Pixi.Text(props.text, {
+      fontFamily: 'PixelMix',
+      padding: 4, // https://github.com/pixijs/pixi.js/issues/4500 -- otherwise on first load the text bounding box is calculated to be too small and the tops of the f's get cut off
+      fontSize: 26, // use 26 then scale down 50% results in sharper letters than 13
+      // align: 'center'
+    });
+    this.text.scale = PixiPointFrom(new Vector2(0.5, 0.5));
+    this.text.x = 10;
+    this.text.y = 10;
+    this.text.zIndex = 2;
+    this.container.addChild(this.text);
+
+    const box = new Pixi.Graphics();
+
+    box.lineStyle(1, 0x222222, 1);
+    box.beginFill(0xEEEEEE);
+    box.drawRoundedRect(0, 0, this.text.width + 18, this.text.height + 18, 4);
+    box.zIndex = 0;
+    this.container.addChild(box);
   }
 
-  protected renderSelf() {
-
+  protected renderSelf(props: Props) {
+    this.container.visible = props.visible;
+    this.container.position = PixiPointFrom(props.position);
   }
 }
 
