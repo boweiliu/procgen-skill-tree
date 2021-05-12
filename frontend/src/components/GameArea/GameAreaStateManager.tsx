cc877535bb4cabe146ead59c2296aa8976ff9232
Bowei Liu <liubowei@gmail.com>
2021-05-12 12:12:54 -0700

    finish small refactor

--- frontend/src/components/GameArea/GameAreaStateManager.tsx
+++ frontend/src/components/GameArea/GameAreaStateManager.tsx
@@ -10,6 +10,7 @@ import { KeyedHashMap } from '../../lib/util/data_structures/hash';
 import { Vector2 } from '../../lib/util/geometry/vector2';
 import { Vector3 } from '../../lib/util/geometry/vector3';
 import { UpdaterGeneratorType2 } from '../../lib/util/updaterGenerator';
+import { computeVirtualNodeDataMap } from './computeVirtualNodeDataMap';
 import {
   GameAreaComponent,
   LockStatus,
@@ -134,96 +135,24 @@ function Component(props: {
     [gameState.playerUI.virtualGridLocation, virtualGridDims]
   );
 
-  const virtualGridStatusMap: KeyedHashMap<
-    Vector2,
-    NodeReactData
-  > = useMemo(() => {
-    const startTime = +new Date();
-    const map = new KeyedHashMap<Vector2, NodeReactData>();
-    for (let row = 0; row < virtualGridDims.x; row++) {
-      for (let col = 0; col < virtualGridDims.y; col++) {
-        const virtualVec = new Vector2(row, col);
-        const location = virtualDimsToLocation(virtualVec);
-        const maybeStatus = gameState.computed.fogOfWarStatusMap?.get(location);
-        const takenStatus = gameState.playerSave.allocationStatusMap.get(
-          location
-        );
-        const nodeStatus =
-          takenStatus === NodeAllocatedStatus.TAKEN
-            ? NodeAllocatedStatus.TAKEN
-            : maybeStatus || NodeAllocatedStatus.HIDDEN;
-        const id = location.hash();
-        const lockData = gameState.worldGen.lockMap.get(location);
-        const nodeContents = gameState.worldGen.nodeContentsMap.get(location);
-        let shortText1 = '+';
-        if (nodeContents.lines[0]) {
-          // the origin node is empty
-          shortText1 += AttributeSymbolMap[nodeContents.lines[0].attribute];
-        } else {
-          shortText1 = '';
-        }
-        if (
-          nodeContents.lines[1] &&
-          nodeContents.lines[0].attribute !== nodeContents.lines[1].attribute
-        ) {
-          // add another symbol if it's a mixed node
-          shortText1 += AttributeSymbolMap[nodeContents.lines[1].attribute];
-        }
-        let shortText2 = '';
-        if (nodeContents.condition) {
-          shortText2 =
-            '-' + AttributeSymbolMap[nodeContents.condition.attribute];
-        }
-
-        const nodeData: NodeReactData = {
-          shortText: shortText2 ? (
-            <>
-              {shortText1}
-              <br />
-              {shortText2}
-            </>
-          ) : (
-            <>{shortText1}</>
-          ),
-          toolTipText: (
-            <>
-              <div>{nodeStatus.toString()}</div>
-              {nodeContents.lines[0] ? <br /> : <></>}
-              <div>
-                {nodeContents.lines[0] &&
-                  nodeContentsLineToString(nodeContents.lines[0])}
-              </div>
-              <div>
-                {nodeContents.lines[1] &&
-                  nodeContentsLineToString(nodeContents.lines[1])}
-              </div>
-              <div>
-                {nodeContents.condition &&
-                  nodeContentsConditionToString(nodeContents.condition)}
-              </div>
-            </>
-          ),
-          fullText: <> </>,
-          status: nodeStatus,
-          lockData,
-          id,
-        };
-        map.put(virtualVec, nodeData);
-      }
-    }
-    // console.log({ map });
-    const elapsed = +new Date() - startTime;
-    if (elapsed > 100) {
-      window.alert('compute took ' + elapsed.toString());
-    }
-    return map;
-  }, [
-    gameState.playerSave.allocationStatusMap,
-    gameState.worldGen.nodeContentsMap,
-    gameState.worldGen.lockMap,
-    virtualGridDims,
-    virtualDimsToLocation,
-  ]);
+  const virtualGridStatusMap: KeyedHashMap<Vector2, NodeReactData> = useMemo(
+    () =>
+      computeVirtualNodeDataMap({
+        allocationStatusMap: gameState.playerSave.allocationStatusMap,
+        nodeContentsMap: gameState.worldGen.nodeContentsMap,
+        lockMap: gameState.worldGen.lockMap,
+        fogOfWarStatusMap: gameState.computed.fogOfWarStatusMap,
+        virtualGridDims,
+        virtualDimsToLocation,
+      }),
+    [
+      gameState.playerSave.allocationStatusMap,
+      gameState.worldGen.nodeContentsMap,
+      gameState.worldGen.lockMap,
+      virtualGridDims,
+      virtualDimsToLocation,
+    ]
+  );
 
   const handleJump = useCallback(
     (args: { direction: Vector2 }) => {
