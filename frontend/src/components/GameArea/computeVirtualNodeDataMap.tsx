88fc87fcc0e1597278ced833f504c982cd28eae9
Bowei Liu <liubowei@gmail.com>
2021-05-23 02:14:23 -0700

    adjust wording copy

--- frontend/src/components/GameArea/computeVirtualNodeDataMap.tsx
+++ frontend/src/components/GameArea/computeVirtualNodeDataMap.tsx
@@ -98,7 +98,7 @@ export function computeVirtualNodeDataMap(args: {
             {location.equals(Vector3.Zero) ? (
               <>
                 {' '}
-                <br /> <div>Your starting node</div>
+                <br /> <div>The starting node</div>
               </>
             ) : (
               <> </>
