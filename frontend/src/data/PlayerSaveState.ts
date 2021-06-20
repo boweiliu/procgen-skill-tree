5c511420dce7991f45c2c2e30d112c756f89ed18
Bowei Liu <liubowei@gmail.com>
2021-06-19 21:51:06 -0700

    remove dead code from player save state

--- frontend/src/data/PlayerSaveState.ts
+++ frontend/src/data/PlayerSaveState.ts
@@ -5,24 +5,9 @@ import { ResourceType } from './WorldGenState';
 
 export type PlayerSaveState = {
   /**
-   * DEPRECATED
-   */
-  activeQuest: Quest | undefined;
-  spSpentThisQuest: number | undefined;
-  questProgressHistory: number[];
-  questInitialAmount: number;
-  score: number;
-  // questsCompleted: Quest[];
-
-  /**
-   * NOT DEPRECATED
-   */
-
-  /**
    * Indicated which nodes are allocated or not. NOTE: does not contain fog of war information
    */
   allocationStatusMap: KeyedHashMap<Vector3, NodeTakenStatus>;
-  // lockMap: LazyHashMap<Vector3, LockData | undefined>;
 };
 
 // DEPRECATED
@@ -41,14 +26,6 @@ export type LockData = {
 
 export const newPlayerSaveState = (): PlayerSaveState => {
   return {
-    // justAllocated: undefined,
-    activeQuest: undefined,
-    spSpentThisQuest: undefined,
-    questProgressHistory: [],
-    questInitialAmount: 0,
-    // questsCompleted: [],
-    score: 0,
-
     // make sure to allocate the beginning node
     allocationStatusMap: new KeyedHashMap<Vector3, NodeTakenStatus>([
       [Vector3.Zero, NodeTakenStatus.true],
