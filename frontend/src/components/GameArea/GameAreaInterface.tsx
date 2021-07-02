5f757a6d7d65a2db36cd35f197056fb847535ccf
Bowei Liu <liubowei@gmail.com>
2021-07-01 17:50:22 -0700

    move around comments

--- frontend/src/components/GameArea/GameAreaInterface.tsx
+++ frontend/src/components/GameArea/GameAreaInterface.tsx
@@ -11,23 +11,6 @@ import { CssVariablesComponent } from './CssVariables';
 import { GameAreaStateManager } from './GameAreaStateManager';
 
 /**
- * Approximations for sqrt(3)/2 == ratio of an equilateral triangle's height to its width:
- * 6/7, 13/15 (*), 26/30, 45/52, 58/67, 84/97 (*), 97/112, 181/209 (*)
- * for divisibility -- recommend 19/22, 26/30, 52/60, 78/90, 104/120, 168/194, 180/208, 232/268, 336/388
- */
-// export const hexGridPx = new Vector2(268, 232);
-// export const hexGridPx = new Vector2(194, 168);
-// export const hexGridPx = new Vector2(120, 104);
-// export const hexGridPx = new Vector2(97, 84);
-// export const hexGridPx = new Vector2(75, 65); // TODO(bowei): change text font size to xx-small
-
-// export const hexCenterRadius = 64; // Radius of the circles representing allocatable nodes, in px
-// export const hexCenterRadius = 44; // Radius of the circles representing allocatable nodes, in px
-// export const hexCenterRadius = Math.round(hexGridPx.x / 16 - 0.025) * 4; // Radius of the circles representing allocatable nodes, in px
-
-// export const borderWidth = hexGridPx.x > 100 ? 2 : 1; // border of circles, etc. in px
-
-/**
  * How much bigger the "virtual" (i.e. scrollable) game area is than the visible window.
  * Bigger == more elements rendered which are outside the viewport == worse performance,
  * but need to 'jump' the scroll viewport less often.
@@ -92,34 +75,75 @@ export function GameAreaInterface(props: {
   }, [gameState.windowState.innerWidth, gameState.windowState.innerHeight]);
 
   // TODO(bowei): programmatically determine UI scale based on app size
-  const uiScale: UiScale = useMemo(() => {
-    if (appSize.x > 1920) {
+  let uiScale: UiScale = useMemo(() => {
+    let maxDim = Math.max(appSize.x, appSize.y);
+    if (maxDim > 2160) {
+      return 'x-large';
+    } else if (maxDim > 1536) {
       return 'large';
-    } else if (appSize.x > 1080) {
+    } else if (maxDim > 1080) {
       return 'medium';
-    } else {
+    } else if (maxDim > 800) {
       return 'small';
+    } else {
+      return 'x-small';
     }
   }, [appSize]);
+
+  /**
+   * Approximations for sqrt(3)/2 == ratio of an equilateral triangle's height to its width:
+   * 6/7, 13/15 (*), 26/30, 45/52, 58/67, 84/97 (*), 97/112, 181/209 (*)
+   * for divisibility -- recommend 19/22, 26/30, 52/60, 78/90, 104/120, 168/194, 180/208, 232/268, 336/388
+   */
   const hexGridPx = useMemo(() => {
-    if (uiScale === 'large') {
+    if (uiScale === 'x-large') {
       return new Vector2(268, 232);
-      // return new Vector2(194, 168);
+    } else if (uiScale === 'large') {
+      return new Vector2(194, 168);
     } else if (uiScale === 'medium') {
       return new Vector2(120, 104);
-    } else {
-      // return new Vector2(97, 84);
+    } else if (uiScale === 'small') {
+      return new Vector2(97, 84);
+    } else if (uiScale === 'x-small') {
       return new Vector2(75, 65); // TODO(bowei): change text font size to xx-small
+    } else {
+      throw new Error(''); // should never get here
     }
   }, [uiScale]);
 
+  // Radius of the circles representing allocatable nodes, in px
   const hexCenterRadius = useMemo(() => {
-    return Math.round(hexGridPx.x / 16 - 0.025) * 4; // Radius of the circles representing allocatable nodes, in px
-  }, [hexGridPx]);
+    if (uiScale === 'x-large') {
+      return 64;
+    } else if (uiScale === 'large') {
+      return 48;
+    } else if (uiScale === 'medium') {
+      return 32;
+    } else if (uiScale === 'small') {
+      return 26;
+    } else if (uiScale === 'x-small') {
+      return 20;
+    } else {
+      throw new Error(''); // should never get here
+    }
+  }, [uiScale]);
 
+  // border of circles, etc. in px
   const borderWidth = useMemo(() => {
-    return hexGridPx.x > 100 ? 2 : 1; // border of circles, etc. in px
-  }, [hexGridPx]);
+    if (uiScale === 'x-large') {
+      return 2;
+    } else if (uiScale === 'large') {
+      return 2;
+    } else if (uiScale === 'medium') {
+      return 2;
+    } else if (uiScale === 'small') {
+      return 1;
+    } else if (uiScale === 'x-small') {
+      return 1;
+    } else {
+      throw new Error(''); // should never get here
+    }
+  }, [uiScale]);
 
   const onDebugRetrigger = gameState.debug.retriggerVirtualGridDims; // triggered from debug tab to check performance
   const virtualGridDims = useMemo(() => {
