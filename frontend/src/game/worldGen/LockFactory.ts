860a1a250ecc95d1dbe3a1c234481ea1e41b4539
Bowei Liu <liubowei@gmail.com>
2021-07-03 23:26:08 -0700

    go back to more open worlds

--- frontend/src/game/worldGen/LockFactory.ts
+++ frontend/src/game/worldGen/LockFactory.ts
@@ -8,7 +8,7 @@ type LockFactoryConfig = {};
 
 // locks occur at this frequency
 // a good non-debug value is 0.47. 0.5 is the site percolation threshold for triangular lattice
-const LOCK_FREQUENCY = 0.485;
+const LOCK_FREQUENCY = 0.465;
 const LOCK_FREQUENCY_STARTER_AREA = 0.3;
 export const STARTER_AREA_RADIUS = 5;
 
