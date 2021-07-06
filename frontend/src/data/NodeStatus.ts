af9609649abdafdb7fad072d81f926e79fb6bfdb
Bowei Liu <liubowei@gmail.com>
2021-07-06 04:02:14 -0700

    add some node statuses to prepare for 2-era gameplay.

--- frontend/src/data/NodeStatus.ts
+++ frontend/src/data/NodeStatus.ts
@@ -8,15 +8,31 @@ export enum NodeAllocatedStatus {
  * taken implies reachable. reachable implies visible.
  */
 
+// Whether or not the node is allocated. implies reachable
 export type NodeTakenStatus = {
   taken: boolean;
 };
-export type NodeVisibleStatus = {
-  visible: boolean;
-};
+// Whether or not the node is within 1 distance from an allocated node. not used during exploration era. implies visible
 export type NodeReachableStatus = {
   reachable: boolean;
 };
+// whether or not the node is revealed in fog of war. implies accessible
+export type NodeVisibleStatus = {
+  visible: boolean;
+};
+// whether or not the node exists in the current era of the game and can be accessed at all.
+export type NodeAccessibleStatus = {
+  accessible: boolean;
+};
+// whether or not the node was ever touched in the exploration era. once explored cannot be unexplored.
+export type NodeExploredStatus = {
+  explored: boolean;
+};
+// whether or not the node was allocated at the end of the exploration era. player can save or unsave nodes and they will be marked for convenience in the optimization era.
+export type NodeSavedStatus = {
+  saved: boolean;
+};
+
 /**
  * Immutable, readable booleans
  */
