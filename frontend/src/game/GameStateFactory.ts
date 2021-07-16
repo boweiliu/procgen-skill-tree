boweiliu/procgen-skill-tree

commit ab84c054114210ece0e32700c76a97a8a44d9551

refactor validlocks and bfsTies code into hexgrid

frontend/src/game/GameStateFactory.ts
@@ -164,18 +164,7 @@ export function markAccessibleNodes(args: {
   // let result: typeof prev | null = null;
 
   // make sure we make use of lock state
-  const validLocks: IReadonlySet<Vector3> = {
-    // TODO(bowei): optimize this?
-    contains: (v: Vector3) => {
-      const lockData = prevGameState.worldGen.lockMap.get(v);
-      const lockStatus = prevGameState.computed.lockStatusMap?.get(v);
-      const isLocked = !!lockData && lockStatus !== LockStatus.OPEN;
-      if (isLocked) {
-        return true;
-      }
-      return false;
-    },
-  };
+  const validLocks = getValidLocks(prevGameState);
 
   getWithinDistance(
     Vector3.Zero,
@@ -300,6 +289,40 @@ export function markVisibleNodes(
   return result || prev;
 }
 
+function _validLocksExtract(gameState: GameState) {
+  return {
+    worldGen: {
+      lockMap: gameState.worldGen.lockMap,
+    },
+    computed: {
+      lockStatusMap: gameState.computed.lockStatusMap,
+      accessibleStatusMap: gameState.computed.accessibleStatusMap,
+    },
+  };
+}
+export type ValidLocksSubState = ReturnType<typeof _validLocksExtract>;
+
+export function getValidLocks(
+  prevGameState: Const<ValidLocksSubState>
+): IReadonlySet<Vector3> {
+  // make sure we make use of lock state
+  const validLocks: IReadonlySet<Vector3> = {
+    // TODO(bowei): optimize this?
+    contains: (v: Vector3) => {
+      const lockData = prevGameState.worldGen.lockMap.get(v);
+      const lockStatus = prevGameState.computed.lockStatusMap?.get(v);
+      const isLocked = !!lockData && lockStatus !== LockStatus.OPEN;
+      const isAccessible =
+        !!prevGameState.computed.accessibleStatusMap?.get(v)?.accessible;
+      if (isLocked || !isAccessible) {
+        return true;
+      }
+      return false;
+    },
+  };
+  return validLocks;
+}
+
 /**
  *
  * @param result mutable temporary storage. null if the state is intended to be the same as prev
@@ -339,20 +362,7 @@ export function flowFogOfWarFromNode(args: {
   });
 
   // make sure we make use of lock state
-  const validLocks: IReadonlySet<Vector3> = {
-    // TODO(bowei): optimize this?
-    contains: (v: Vector3) => {
-      const lockData = prevGameState.worldGen.lockMap.get(v);
-      const lockStatus = prevGameState.computed.lockStatusMap?.get(v);
-      const isLocked = !!lockData && lockStatus !== LockStatus.OPEN;
-      const isAccessible =
-        !!prevGameState.computed.accessibleStatusMap?.get(v)?.accessible;
-      if (isLocked || !isAccessible) {
-        return true;
-      }
-      return false;
-    },
-  };
+  const validLocks = getValidLocks(prevGameState);
 
   // flow fog of war, keeping in mind validLocks and accessibility restrictions
   getWithinDistance(nodeLocation, FOG_OF_WAR_DISTANCE, 0, validLocks).forEach(

frontend/src/components/GameArea/GameAreaGrid.tsx
@@ -15,7 +15,12 @@ import { extractDeps } from '../../lib/util/misc';
 import { AllocateNodeResult } from '../../game/actions/AllocateNode';
 import { GameState } from '../../data/GameState';
 import { HashMap, HashSet } from '../../lib/util/data_structures/hash';
-import { getCoordNeighbors, IReadonlySet } from '../../game/lib/HexGrid';
+import {
+  bfsAllPaths,
+  getCoordNeighbors,
+  IReadonlySet,
+} from '../../game/lib/HexGrid';
+import { getValidLocks } from '../../game/GameStateFactory';
 
 /**
  * The subset of the game state that is relevant to game area components.
@@ -106,120 +111,29 @@ function Component(props: {
   }, [gameState]);
 
   const nodesInHoverPathMap = useMemo(() => {
-    const result = new HashSet<Vector3>();
-
     if (!gameState.intent.activeIntent.TEMP_SHOW_PATHS) {
-      return result;
+      return new HashSet<Vector3>();
     }
 
     const target = gameState.playerUI.hoverPathTarget;
-
     if (!target) {
-      return result;
+      return new HashSet<Vector3>();
     }
 
-    // TODO(bowei): compute shortest path from node hover/selection to allocated sector (DFS)
-    // TODO(bowei); replace this boilerplate
-    // make sure we make use of lock state
-    const validLocks: IReadonlySet<Vector3> = {
-      // TODO(bowei): optimize this?
-      contains: (v: Vector3) => {
-        const lockData = gameState.worldGen.lockMap.get(v);
-        const lockStatus = gameState.computed.lockStatusMap?.get(v);
-        const isLocked = !!lockData && lockStatus !== LockStatus.OPEN;
-        const isAccessible =
-          !!gameState.computed.accessibleStatusMap?.get(v)?.accessible;
-        if (isLocked || !isAccessible) {
-          return true;
-        }
-        return false;
-      },
-    };
-    {
-      let source = target;
-      let destinations = new HashSet<Vector3>(
+    // compute shortest path from node hover/selection to allocated sector (DFS)
+    return bfsAllPaths({
+      source: target, // BFS backwards for more efficiency
+      destinations: new HashSet<Vector3>(
         gameState.playerSave.allocationStatusMap
           .entries()
           .filter(([v, status]) => {
             return status.taken === true;
           })
           .map((it) => it[0])
-      );
-      let shortestPathDist = Infinity;
-
-      if (destinations.contains(source)) {
-        result.put(source);
-        return result;
-      }
-
-      let touched = new HashMap<Vector3, [number, HashSet<Vector3>]>();
-      touched.put(source, [0, new HashSet()]);
-      let queue = [source];
-      while (queue.length) {
-        // let currentPath = queue.shift()!;
-        // let currentNode = currentPath[currentPath.length - 1];
-        let currentNode = queue.shift()!;
-        let [currentDist] = touched.get(currentNode)!; // state: we have already examined currentNode and now need to process its neighbors
-        // if (touched.contains(currentNode)) { continue; }
-
-        if (currentDist >= shortestPathDist) {
-          // we found everything; now just need to extract data
-
-          // first iterate through destinations that are in the touched set
-          let considering = destinations
-            .values()
-            .filter((it) => touched.contains(it));
-          while (considering.length) {
-            considering.forEach((it) => result.put(it));
-            let newConsidering = new HashSet<Vector3>();
-            considering.forEach((it) =>
-              touched
-                .get(it)?.[1]
-                .values()
-                .forEach((predecessor) => newConsidering.put(predecessor))
-            );
-            considering = newConsidering.values();
-          }
-
-          return result;
-        }
-
-        const nbors = Object.values(getCoordNeighbors(currentNode));
-        for (let nbor of nbors) {
-          const maybeGotThereAlready = touched.get(nbor);
-          if (maybeGotThereAlready) {
-            // we got there already. still need to determine if we got there just as quickly through this route, and if so, record predecessors
-            if (maybeGotThereAlready[0] === currentDist + 1) {
-              // we are tied; append to the predecessors
-              maybeGotThereAlready[1].put(currentNode);
-            }
-            continue;
-          }
-
-          touched.put(nbor, [currentDist + 1, new HashSet([currentNode])]);
-
-          if (validLocks.contains(nbor)) {
-            continue;
-          }
-
-          // const newPath = [...currentPath, nbor];
-          if (destinations.contains(nbor)) {
-            // we have found a shortest path, now to find the rest
-            // shortestPathDist = newPath.length;
-            shortestPathDist = currentDist + 1;
-            // record it
-            // newPath.forEach(it => result.put(it));
-            // return result;
-          }
-
-          queue.push(nbor);
-        }
-        // touched.put(currentNode);
-      }
-      console.log('did not find a valid path!');
-    }
-
-    return result;
+      ),
+      // make sure we make use of lock state
+      validLocks: getValidLocks(gameState),
+    });
   }, [gameState]);
 
   /**

frontend/src/game/lib/HexGrid.ts
@@ -1,4 +1,4 @@
-import { HashSet } from '../../lib/util/data_structures/hash';
+import { HashMap, HashSet } from '../../lib/util/data_structures/hash';
 import { Vector3 } from '../../lib/util/geometry/vector3';
 
 export enum Direction {
@@ -117,5 +117,105 @@ export function getWithinDistance(
     }
   }
   result = result.concat(disallowedButTouched.values());
+  return result;
+}
+
+/**
+ * runs BFS from source to destinations, returning the union of all ties for shortest paths
+ * @param source the node to start BFS from
+ * @param destinations the set of nodes we are trying to reach
+ * @param validLocks the set of nodes we are not allowed to go through
+ * @returns finds all shortest paths with the same shortest path distance, and returns the union of all nodes contained in those paths
+ * includes the source node and all valid destination nodes.
+ */
+export function bfsAllPaths(args: {
+  source: Vector3;
+  destinations: HashSet<Vector3>;
+  validLocks: IReadonlySet<Vector3>;
+}): HashSet<Vector3> {
+  const { source, destinations, validLocks } = args;
+
+  // let source = target;
+  // let destinations = new HashSet<Vector3>(
+  //   gameState.playerSave.allocationStatusMap
+  //     .entries()
+  //     .filter(([v, status]) => {
+  //       return status.taken === true;
+  //     })
+  //     .map((it) => it[0])
+  // );
+  let shortestPathDist = Infinity;
+  const result = new HashSet<Vector3>();
+
+  if (destinations.contains(source)) {
+    result.put(source);
+    return result;
+  }
+
+  let touched = new HashMap<Vector3, [number, HashSet<Vector3>]>();
+  touched.put(source, [0, new HashSet()]);
+  let queue = [source];
+  while (queue.length) {
+    // let currentPath = queue.shift()!;
+    // let currentNode = currentPath[currentPath.length - 1];
+    let currentNode = queue.shift()!;
+    let [currentDist] = touched.get(currentNode)!; // state: we have already examined currentNode and now need to process its neighbors
+    // if (touched.contains(currentNode)) { continue; }
+
+    if (currentDist >= shortestPathDist) {
+      // we found everything; now just need to extract data
+
+      // first iterate through destinations that are in the touched set
+      let considering = destinations
+        .values()
+        .filter((it) => touched.contains(it));
+      while (considering.length) {
+        considering.forEach((it) => result.put(it));
+        let newConsidering = new HashSet<Vector3>();
+        considering.forEach((it) =>
+          touched
+            .get(it)?.[1]
+            .values()
+            .forEach((predecessor) => newConsidering.put(predecessor))
+        );
+        considering = newConsidering.values();
+      }
+
+      return result;
+    }
+
+    const nbors = Object.values(getCoordNeighbors(currentNode));
+    for (let nbor of nbors) {
+      const maybeGotThereAlready = touched.get(nbor);
+      if (maybeGotThereAlready) {
+        // we got there already. still need to determine if we got there just as quickly through this route, and if so, record predecessors
+        if (maybeGotThereAlready[0] === currentDist + 1) {
+          // we are tied; append to the predecessors
+          maybeGotThereAlready[1].put(currentNode);
+        }
+        continue;
+      }
+
+      touched.put(nbor, [currentDist + 1, new HashSet([currentNode])]);
+
+      if (validLocks.contains(nbor)) {
+        continue;
+      }
+
+      // const newPath = [...currentPath, nbor];
+      if (destinations.contains(nbor)) {
+        // we have found a shortest path, now to find the rest
+        // shortestPathDist = newPath.length;
+        shortestPathDist = currentDist + 1;
+        // record it
+        // newPath.forEach(it => result.put(it));
+        // return result;
+      }
+
+      queue.push(nbor);
+    }
+    // touched.put(currentNode);
+  }
+  console.log('did not find a valid path!');
   return result;
 }

commit 674f5ae69dac79b6955920840fced00a6c60472c

fixup: when refactored ab84c05, accidentally broke accessible flow

frontend/src/game/GameStateFactory.ts
@@ -163,8 +163,18 @@ export function markAccessibleNodes(args: {
 
   // let result: typeof prev | null = null;
 
-  // make sure we make use of lock state
-  const validLocks = getValidLocks(prevGameState);
+  // make sure we make use of lock state, but NOT accessible state
+  const validLocks: IReadonlySet<Vector3> = {
+    contains: (v: Vector3) => {
+      const lockData = prevGameState.worldGen.lockMap.get(v);
+      const lockStatus = prevGameState.computed.lockStatusMap?.get(v);
+      const isLocked = !!lockData && lockStatus !== LockStatus.OPEN;
+      if (isLocked) {
+        return true;
+      }
+      return false;
+    },
+  };
 
   getWithinDistance(
     Vector3.Zero,
@@ -362,7 +372,7 @@ export function flowFogOfWarFromNode(args: {
   });
 
   // make sure we make use of lock state
-  const validLocks = getValidLocks(prevGameState);
+  const validLocks: IReadonlySet<Vector3> = getValidLocks(prevGameState);
 
   // flow fog of war, keeping in mind validLocks and accessibility restrictions
   getWithinDistance(nodeLocation, FOG_OF_WAR_DISTANCE, 0, validLocks).forEach(