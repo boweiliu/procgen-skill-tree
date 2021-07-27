a19736cca341057426ab4257fceeebabff18f7f7
Bowei Liu <liubowei@gmail.com>
2021-07-27 16:36:06 -0700

    add pyqt so things display on linux

--- simulation/noise1d/setup.sh
+++ simulation/noise1d/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# ensure these pip packages on linux
+# sudo apt-get install python3-tk python3-venv python3
+
 python3 -m venv .venv
 source ./.venv/bin/activate
 pip freeze > requirements.txt
