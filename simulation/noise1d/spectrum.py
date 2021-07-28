d1f0f44070ea388ad6f9ea0826d9e41ce4cb82e4
Bowei Liu <liubowei@gmail.com>
2021-07-28 16:17:20 -0700

    fixup

--- simulation/noise1d/spectrum.py
+++ simulation/noise1d/spectrum.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 
 import numpy as np
 import scipy
@@ -17,10 +18,11 @@ if __name__ == '__main__':
     #plt.plot(xf, np.abs(yf))
 
     # compute squared power, and divide into buckets
-    # also we should normalize so we can show percentage power
     unbucketed_power_f = np.abs(yf) * np.abs(yf) 
-    bucketed_pp_f = np.array([ 1/N * np.sum(unbucketed_power_f[ i * BUCKET_SIZE : ( i+1 ) * BUCKET_SIZE]) for i in range(NUM_BUCKETS) ]) # percent power
+    bucketed_p_f = np.array([ np.sum(unbucketed_power_f[ i * BUCKET_SIZE : ( i+1 ) * BUCKET_SIZE]) for i in range(NUM_BUCKETS) ]) # percent power
     bucketed_xf = np.array([ 1/BUCKET_SIZE * np.sum(xf[i * BUCKET_SIZE:(i+1) * BUCKET_SIZE]) for i in range(NUM_BUCKETS) ]) # avg
+    # also we should normalize so on average each data point == 1
+    bucketed_pp_f = bucketed_p_f * 1/N * NUM_BUCKETS
 
     plt.plot(bucketed_xf, bucketed_pp_f)
     plt.show()
