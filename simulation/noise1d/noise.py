34f848183b6e5eaa8fa5708341581ba9e99af8ab
Bowei Liu <liubowei@gmail.com>
2021-07-28 16:51:50 -0700

    mid refactor, still works

--- simulation/noise1d/noise.py
+++ simulation/noise1d/noise.py
@@ -2,16 +2,16 @@
 import numpy as np
 from util import N, SAMPLE_RATE, DURATION
 
-def generate_bernoulli_noise():
+def generate_bernoulli_noise(iterations = 1):
     xs = np.linspace(0, DURATION, N, endpoint=False)
-    ys = np.random.randint(2, size=len(xs)) # 0 or 1
+    ys = np.random.randint(2, size=(len(xs), iterations)) # 0 or 1
     ys = ys * 2 - 1 # -1 or 1
     return xs, ys
 
 if __name__ == '__main__':
     from matplotlib import pyplot as plt
     x, y = generate_bernoulli_noise()
-    plt.plot(x[:100], y[:100])
+    plt.plot(x[:100], y[:100,0])
     plt.show()
 
 
--- simulation/noise1d/spectrum.py
+++ simulation/noise1d/spectrum.py
@@ -14,15 +14,18 @@ BUCKET_SIZE = int(N /2 / NUM_BUCKETS)
 
 if __name__ == '__main__':
     xs, ys = generate_bernoulli_noise()
-    yf = nfft(ys)
+    #yf = nfft(ys[:,0], axis=0)
+    yf = nfft(ys, axis=0)[:,0]
     xf = fftfreq(N, 1 / SAMPLE_RATE) # 0 .... 4999, -5000, .... -1 order
     #plt.plot(xf, np.abs(yf))
 
     # computed squared power
     unbucketed_power_f = np.abs(yf) * np.abs(yf) 
     # divide inot buckets and cut off the redundant negative frequencies
+    
     bucketed_p_f = np.array([ np.sum(unbucketed_power_f[ i * BUCKET_SIZE : ( i+1 ) * BUCKET_SIZE]) for i in range(NUM_BUCKETS) ]) # percent power
-    bucketed_xf = np.array([ 1/BUCKET_SIZE * np.sum(xf[i * BUCKET_SIZE:(i+1) * BUCKET_SIZE]) for i in range(NUM_BUCKETS) ]) # avg
+    #bucketed_xf = np.array([ 1/BUCKET_SIZE * np.sum(xf[i * BUCKET_SIZE:(i+1) * BUCKET_SIZE]) for i in range(NUM_BUCKETS) ]) # avg
+    bucketed_xf = np.mean((xf[0:N//2]).reshape(NUM_BUCKETS, BUCKET_SIZE), axis=1)
     # also we should normalize so on average each data point == 1
     bucketed_pp_f = bucketed_p_f / BUCKET_SIZE
 
