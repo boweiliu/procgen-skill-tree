4823f658aa992547da221ce20985b47a1c9bc315
Bowei Liu <liubowei@gmail.com>
2021-07-30 15:52:23 -0700

    try quantizing brownian noise

--- simulation/noise1d/noise.py
+++ simulation/noise1d/noise.py
@@ -56,7 +56,7 @@ def generate_brownian_warmstart_noise(iterations = 1):
     ys = ys/np.sqrt((2*N + N+1)/2)
     return xs, ys
 
-# other types of brown noise
+# normalize both endpoints to 0 and the power to exactly 1
 def generate_brownian_normalized_noise(iterations = 1):
     xs = np.linspace(0, DURATION, N, endpoint=False)
     ds = np.random.randint(2, size=(N * 2, iterations)) # 0 or 1
@@ -82,6 +82,23 @@ def generate_brownian_quantized_noise(iterations = 1):
     ys = np.where(np.greater_equal(ys, 0),1,-1)
     return xs, ys
     
+# normalize both endpoints to 0 and the power to exactly 1. THEN quantize/clamp
+def generate_brownian_normalized_clamped_noise(iterations = 1):
+    xs = np.linspace(0, DURATION, N, endpoint=False)
+    ds = np.random.randint(2, size=(N * 2, iterations)) # 0 or 1
+    ds = ds * 2 - 1 # -1 or 1
+    #padding = np.array([0 for _ in range(iterations)]).reshape((1, iterations))
+    #ds = np.concatenate((padding, ds), axis=0)
+    ys = np.cumsum(ds, axis=0)
+    ys = ys[N:2 * N]
+# normalize
+    lin = np.linspace(ys[0], ys[N-1], N, endpoint=True)
+    ys = np.subtract(ys, lin)
+    power = np.mean(ys * ys, axis=0)
+    ys = np.divide(ys, np.sqrt(power))
+    ys = np.where(np.greater_equal(ys, 0),1,-1)
+    return xs, ys
+
 # TODO: start with bb but normalize so t=0 and t=9 are both 0, and normalize power to 1 per-iteration
 # TODO: like 2way but instead of starting on the 2 ends, also start in the middle, and at the quartiles, etc.?
 
@@ -102,8 +119,9 @@ if __name__ == '__main__':
 
     # x, y = generate_blue_bernoulli_noise(100)
     #x, y = generate_brownian_normalized_noise(100)
-    x, y = generate_brownian_quantized_noise(100)
+    #x, y = generate_brownian_quantized_noise(100)
     # x, y = generate_brownian_bernoulli_noise(100)
+    x, y = generate_brownian_normalized_clamped_noise(100)
     #plt.plot(x[:100], y[:100,0])
     plt.plot(x,y[:,0])
     plt.show()
