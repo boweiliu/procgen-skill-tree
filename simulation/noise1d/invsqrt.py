fb27a8bf168ffdd955b71dfbc04fe6a4495b11aa
Bowei Liu <liubowei@gmail.com>
2021-08-06 16:09:25 -0700

    add blue noise and normalize

--- simulation/noise1d/invsqrt.py
+++ simulation/noise1d/invsqrt.py
@@ -11,12 +11,13 @@ import scipy.signal as signal
 from util import DURATION, N
 from spectrum import NUM_BUCKETS
 #from brownian_quantization import plot_helper
-from blur_filter import white_bernoulli_powernormalized
+from blur_filter import white_bernoulli_powernormalized, diff_window
 
 def main():
     plot_helper(white_bernoulli_powernormalized)
     plot_helper(white_bernoulli_invsqrtblur300)
     plot_helper(white_bernoulli_invsqrt_accumulate)
+    plot_helper(white_bernoulli_invsqrt_diff)
     #plot_helper(white_bernoulli_diff)
     #plot_helper(white_bernoulli_gaussblur9)
     plt.legend()
@@ -29,25 +30,32 @@ def main():
 def test():
     #x, y = white_bernoulli_powernormalized(100)
     x, y = white_bernoulli_invsqrt_accumulate(100)
+    #x, y = white_bernoulli_invsqrt_diff(100)
     #x, y = white_bernoulli_gaussblur9(100)
     #plt.plot(x[:100], y[:100, 0])
-    #plt.plot(x[:], y[:, 0])
-    #plt.show()
-    #print(np.mean(np.mean(y * y, axis=0)))
-    #plt.clf()
+    print(np.mean(np.mean(y * y, axis=0)))
+    plt.plot(x[:], y[:, 0])
+    plt.show()
+    plt.clf()
 
     fig = plt.figure()
     def anim(i):
         plt.clf()
-        plt.ylim(-0.01, 0.01)
-        plt.plot(x[100 * i:100*(i+5)], y[100 * i:100*(i+5), 0])
+        plt.ylim(-0.005, 0.005)
+        #plt.plot(x[100 * i:100*(i+5)], y[100 * i:100*(i+5), 0])
+        plt.plot(x[50 * i:50*(i+5)], y[50 * i:50*(i+5), 0])
         #plt.show()
-    animator = ani.FuncAnimation(fig, anim, interval = 100)
+    animator = ani.FuncAnimation(fig, anim, interval = 50)
     plt.show()
 
 def white_bernoulli_invsqrtblur3(iterations = 1, window=3):
     xs, ys = white_bernoulli_powernormalized(iterations)
     ys = signal.fftconvolve(ys, invsqrt_window(window, 1, iterations), mode='full', axes=0)[:N]
+# normalize power
+    yavg = np.mean(ys, axis=0)
+    ys = ys - yavg
+    ypow = np.mean(ys * ys, axis=0)
+    ys = np.divide(ys, np.sqrt(ypow))
     return xs, ys
 
 def invsqrt_window(window = 3, sigma = 1, iterations = 1):
@@ -61,6 +69,16 @@ def white_bernoulli_invsqrtblur300(iterations = 1):
 
 def white_bernoulli_invsqrt_accumulate(iterations = 1):
     return white_bernoulli_invsqrtblur3(iterations, window=N)
+
+def white_bernoulli_invsqrt_diff(iterations = 1):
+    xs, ys = white_bernoulli_invsqrt_accumulate(iterations)
+    ys = signal.fftconvolve(ys, diff_window(1, 1, iterations), mode='full', axes=0)[:N]
+# normalize power
+    yavg = np.mean(ys, axis=0)
+    ys = ys - yavg
+    ypow = np.mean(ys * ys, axis=0)
+    ys = np.divide(ys, np.sqrt(ypow))
+    return xs, ys
 
 def plot_helper(fn, label=''):
     x, y = spectrum.generate_bucketed_spectrum(fn)
