91b40f626038c937d592d898aa18f93415601257
Bowei Liu <liubowei@gmail.com>
2021-08-10 11:16:32 -0700

    wip set up for upsampling

--- simulation/noise1d/upsampling_quantization.py
+++ simulation/noise1d/upsampling_quantization.py
@@ -10,12 +10,11 @@ import scipy
 import scipy.signal as signal
 from util import DURATION, N
 from spectrum import NUM_BUCKETS
-#from brownian_quantization import plot_helper
-from blur_filter import white_bernoulli_powernormalized, diff_window, white_bernoulli_diff
-from brownian_quantization import brown_bernoulli_2waynormalized
 
 def main():
-    plot_helper(white_bernoulli_powernormalized)
+    plot_helper(gaussian_white)
+    plot_helper(gaussian_brown)
+    plot_helper(gaussian_violet)
 
     plt.legend()
     #mng = plt.get_current_fig_manager()
@@ -25,15 +24,12 @@ def main():
     plt.show()
 
 def test():
-    #x, y = white_bernoulli_powernormalized(100)
-    #x, y = brown_bernoulli_2waynormalized(100)
-    #x, y = white_bernoulli_invsqrt_accumulate(100)
-    x, y = white_bernoulli_invsqrt_diff(100)
-    #x, y = white_bernoulli_diff(100)
-    #x, y = white_bernoulli_gaussblur9(100)
+    #x, y = gaussian_white(100)
+    #x, y = gaussian_brown(100)
+    x, y = gaussian_violet(100)
     print(np.mean(np.mean(y * y, axis=0)))
-    #plt.plot(x[:100], y[:100, 0])
-    plt.plot(x[100:150], y[100:150, 0])
+    plt.plot(x[:100], y[:100, 0])
+    #plt.plot(x[100:150], y[100:150, 0])
     #plt.plot(x[:], y[:, 0])
     plt.show()
     plt.clf()
@@ -48,22 +44,58 @@ def test():
     animator = ani.FuncAnimation(fig, anim, interval = 100)
     plt.show()
 
-def gaussian_white(iterations = 1):
-def gaussian_pink(iterations = 1):
-def gaussian_pink_warm(iterations = 1):
-def gaussian_pink_warm2way(iterations = 1):
-def gaussian_brown(iterations = 1):
-def gaussian_brown_2way(iterations = 1):
-def gaussian_azure(iterations = 1):
-def gaussian_azure_warm(iterations = 1):
-def gaussian_violet(iterations = 1):
+def gaussian_white(iterations = 1, base = 'gaussian'):
+    xs = np.linspace(0, DURATION, N, endpoint=False)
+    if base == 'bernoulli':
+        ys = np.random.randint(2, size=(N, iterations)) # 0 or 1
+        ys = ys * 2 - 1  # -1 or 1
+    elif base == 'gaussian':
+        ys = np.random.normal(0, 1, size=(N, iterations))
+    return xs, ys
 
-def plot_helper(fn, label=''):
+def gaussian_pink(iterations = 1):
+    pass
+
+def gaussian_pink_warm(iterations = 1):
+    pass
+
+def gaussian_pink_warm2way(iterations = 1):
+    pass
+
+def gaussian_brown(iterations = 1):
+    xs, ds = gaussian_white(iterations)
+    ys = np.cumsum(ds, axis=0)
+    ys = normalize(ys)
+    return xs, ys
+
+def gaussian_brown_2way(iterations = 1):
+    pass
+
+def gaussian_azure(iterations = 1):
+    pass
+
+def gaussian_azure_warm(iterations = 1):
+    pass
+
+def gaussian_violet(iterations = 1):
+    xs, ds = gaussian_white(iterations)
+    ys = np.diff(ds, axis=0, prepend=0)
+    ys = normalize(ys)
+    return xs, ys
+
+def normalize(data):
+    yavg = np.mean(data, axis=0)
+    ys = data - yavg
+    ypow = np.mean(ys * ys, axis=0)
+    ys = ys / np.sqrt(ypow)
+    return ys
+
+def plot_helper(fn, label='log-log'):
     x, y = spectrum.generate_bucketed_spectrum(fn)
     #plt.plot(x[2:NUM_BUCKETS//1], y[2:NUM_BUCKETS//1], label=fn.__name__ + ' ' + label)
     #plt.plot(x[2:NUM_BUCKETS//1], np.log(y[2:NUM_BUCKETS//1]), label=fn.__name__ + ' ' + label)
-    #plt.plot(np.log(x[2:NUM_BUCKETS//1]), np.log(y[2:NUM_BUCKETS//1]), label=fn.__name__ + ' ' + label)
-    plt.plot(np.log(x[2:NUM_BUCKETS//2]), np.log(y[2:NUM_BUCKETS//2]), label=fn.__name__ + ' ' + label)
+    plt.plot(np.log(x[2:NUM_BUCKETS//1]), np.log(y[2:NUM_BUCKETS//1]), label=fn.__name__ + ' ' + label)
+    #plt.plot(np.log(x[2:NUM_BUCKETS//2]), np.log(y[2:NUM_BUCKETS//2]), label=fn.__name__ + ' ' + label)
     #plt.plot(np.log(x[2:NUM_BUCKETS//2]), np.log(np.abs(np.log(y[2:NUM_BUCKETS//2]))), label=fn.__name__ + ' ' + label)
 
 if __name__ == '__main__':
